8198911e5fe02ef657b25be31dd41165a1bdcaf4
ComercialAlly <comercial@allymidia.com.br>
2025-10-14 15:30:32 -0300

    Updated whatsapp.ts

--- src/utils/whatsapp.ts
+++ src/utils/whatsapp.ts
@@ -1,6 +1,6 @@
 const whatsappLinks = [
   'https://wa.me/5511977630591?text=Ol%C3%A1%2C%20vim%20pelo%20site%2C%20tudo%20bem%20por%20ai%3F',
-  'https://wa.me/5511950554193?text=Ol%C3%A1%2C%20tudo%20bem%3F%20vim%20pelo%20site%2C%20gostaria%20de%20transformar%20minha%20empresa%20em%20uma%20m%C3%A1quina%20de%20vendas',
+  'https://wa.me/5511916374949?text=Ol%C3%A1%2C%20tudo%20bem%3F%20vim%20pelo%20site%2C%20gostaria%20de%20transformar%20minha%20empresa%20em%20uma%20m%C3%A1quina%20de%20vendas',
 ];
 
 let currentIndex = 0;
